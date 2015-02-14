71162f10c29c54054665bb0715d4df34bba3c8ee
nightwing <amirjanyan@gmail.com>
2015-02-15 02:00:58 +0400

    remove incorrect license information from comments

--- test.js
+++ test.js
@@ -2,7 +2,6 @@
  * Test Panel for the Cloud9
  *
  * @copyright 2010, Ajax.org B.V.
- * @license GPLv3 <http://www.gnu.org/licenses/gpl.txt>
  */
 define(function(require, exports, module) {
     main.consumes = [
